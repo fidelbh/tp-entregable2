3cdd42b07b263a934148497f5f151620738ec4a0
Carolina Navarrete <caro.45navarrete@gmail.com>
2022-10-09 16:08:53 -0300

    carruceles andando

--- js/controlador.js
+++ js/controlador.js
@@ -1,14 +1,44 @@
 'Use strict';
 
-let fila = document.querySelector('.content-carrusel');
+let fila = document.querySelectorAll('.content-carrusel');
+//let filaImagenes = document.querySelector('.contenedor-carrusel');
 let cards = document.querySelector('.carrusel');
-let flechaIzquierda= document.querySelector('#flecha-izquierda');
-let flechaDerecha= document.querySelector('#flecha-derecha');
+let flechaIzquierda= document.querySelectorAll('#flecha-izquierda');
+let flechaDerecha= document.querySelectorAll('#flecha-derecha');
+    
+    for (let index = 0; index <flechaDerecha.length; index++) {
+        for(let j=0; j<fila.length;j++){
+            flechaDerecha[index].addEventListener('click',()=>{
+                fila[j].scrollLeft +=fila[j].offsetWidth;
 
-flechaDerecha.addEventListener('click',()=>{
-    fila.scrollLeft +=fila.offsetWidth;
-})
+         });
+        }
+        }
+        for (let index = 0; index <flechaIzquierda.length; index++) {
+            for(let j=0; j<fila.length;j++){
+                flechaIzquierda[index].addEventListener('click',()=>{
+                    fila[j].scrollLeft -=fila[j].offsetWidth;
+    
+            
+        
+        
+             });
+            }
+            }
 
-flechaIzquierda.addEventListener('click',()=>{
-    fila.scrollLeft -=fila.offsetWidth;
-})
+
+   
+   
+
+/*
+
+    flechaIzquierda.addEventListener('click',()=>{
+        for (let index = 0; index < flechaDerecha.length; index++) {
+        for(let x=0; x=x<fila.length;x++){
+            console.log('hola');
+        fila[x].scrollLeft -=fila.offsetWidth;
+    }
+}
+});
+
+*/
