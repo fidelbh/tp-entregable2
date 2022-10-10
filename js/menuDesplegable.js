429f57dbf0a3694d9e98b47d0448f26edeed40aa
Fidel Brasesco <fidelbrasesco@gmail.com>
2022-10-10 14:23:41 -0300

    menu hamburguesa

---
+++ js/menuDesplegable.js
@@ -0,0 +1,10 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", iniciarPagina);
+function iniciarPagina() {
+  let menu = document.getElementById("menu-hamburguesa");
+  menu.addEventListener("click", monstrarMenu);
+
+  function mostrarMenu() {
+    document.querySelector;
+  }
+}
